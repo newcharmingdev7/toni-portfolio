eafa6d55256aa2f268a8cac11012f629d0cb21da
newcharmingdev7 <tonistuckey8@gmail.com>
2025-11-02 15:34:53 -0500

    Update Footer.js

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,9 +4,6 @@ const Footer = () => {
       <span>Copyright</span>
       <strong className="px-1 sitename">Toni Stuckey</strong>
       <span>All Rights Reserved</span>
-      <div className="credits">
-        Designed by <a href="https://www.squarespace.com/">Squarespace</a>
-      </div>
       <a
         href="#"
         id="scroll-top"
